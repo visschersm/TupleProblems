1723d76605d885bd51ba104a3da2145c0aa14926
Mark Visschers <visschers.m@gmail.com>
2025-03-07 11:18:41 +0100

    setting up test case

---
+++ ClassLibrary1/FrameworkVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public partial class Class1
+    {
+        public List<(int, string)> TupleMethod()
+        {
+            return new List<(int, string)>()
+            {
+                (1, "one"),
+                (2, "two"),
+                (3, "three"),
+            };
+        }
+    }
+}
